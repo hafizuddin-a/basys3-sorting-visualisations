b9804b8517fc5a79e49e4e45e9ef418411152fd3
Hafizuddin Bin Aminuddin <hafizuddinaminuddin1@gmail.com>
2024-03-22 12:01:17 +0800

    Add run_implementation script

--- scripts/create_project.tcl
+++ scripts/create_project.tcl
@@ -16,14 +16,5 @@ launch_runs impl_1 -to_step write_bitstream -jobs 8
 # Wait for the process to complete
 wait_on_run impl_1
 
-# Program the board
-#open_hw_manager
-#connect_hw_server
-#open_hw_target
-#current_hw_device [lindex [get_hw_devices] 0]
-#refresh_hw_device -update_hw_probes false [lindex [get_hw_devices] 0]
-#set_property PROGRAM.FILE {./my_project.runs/impl_1/top_module.bit} [lindex [get_hw_devices] 0]
-#program_hw_devices [lindex [get_hw_devices] 0]
-
 # Close the project
 close_project
--- scripts/run_implementation.tcl
+++ scripts/run_implementation.tcl
@@ -2,7 +2,10 @@
 # and the Vivado project is located in the project_root directory
 
 # Open the existing project
-open_project ../my_project/my_project.xpr
+open_project my_project/my_project.xpr
+
+# Reset the implementation run
+reset_run impl_1
 
 # Run synthesis and implementation
 launch_runs impl_1 -jobs 8
@@ -15,6 +18,15 @@ launch_runs impl_1 -to_step write_bitstream -jobs 8
 
 # Wait for the bitstream generation to complete
 wait_on_run impl_1
+
+# Program the board. Uncomment below to program
+#open_hw_manager
+#connect_hw_server
+#open_hw_target
+#current_hw_device [lindex [get_hw_devices] 0]
+#refresh_hw_device -update_hw_probes false [lindex [get_hw_devices] 0]
+#set_property PROGRAM.FILE {./my_project.runs/impl_1/top_module.bit} [lindex [get_hw_devices] 0]
+#program_hw_devices [lindex [get_hw_devices] 0]
 
 # Close the project
 close_project
