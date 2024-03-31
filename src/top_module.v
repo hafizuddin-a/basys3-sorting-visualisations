e729f1bdf7d90cca184045f54126d0e84c0e6210
JackieNeoCEG <e0959140@u.nus.edu>
2024-04-01 01:49:55 +0800

    goofy aah selection sorting??

--- src/top_module.v
+++ src/top_module.v
@@ -5,13 +5,21 @@ module top_module (
     input clk,
     input sw0,
     input sw1,
+    input sw2,
     output [7:0] Jx
 );
 
-bubble_sort bubble_sort_inst (
+//bubble_sort bubble_sort_inst (
+//    .clk(clk),
+//    .sw0(sw0),
+//    .sw1(sw1),
+//    .Jx(Jx)
+//);
+
+jselection selection_sort_inst(
     .clk(clk),
     .sw0(sw0),
-    .sw1(sw1),
+    .sw2(sw2),
     .Jx(Jx)
 );
 
