f607d2667a812454202616aebf63f73e1742ecda
Chong Xern <104547209+ChongXern@users.noreply.github.com>
2024-04-01 20:27:54 +0800

    Add seven-seg display for  sorting algorithms

--- constraints/constraints.xdc
+++ constraints/constraints.xdc
@@ -47,28 +47,28 @@ set_property -dict { PACKAGE_PIN V16   IOSTANDARD LVCMOS33 } [get_ports {sw1}]
 
 
 ##7 Segment Display
-#set_property -dict { PACKAGE_PIN W7   IOSTANDARD LVCMOS33 } [get_ports {seg[0]}]
-#set_property -dict { PACKAGE_PIN W6   IOSTANDARD LVCMOS33 } [get_ports {seg[1]}]
-#set_property -dict { PACKAGE_PIN U8   IOSTANDARD LVCMOS33 } [get_ports {seg[2]}]
-#set_property -dict { PACKAGE_PIN V8   IOSTANDARD LVCMOS33 } [get_ports {seg[3]}]
-#set_property -dict { PACKAGE_PIN U5   IOSTANDARD LVCMOS33 } [get_ports {seg[4]}]
-#set_property -dict { PACKAGE_PIN V5   IOSTANDARD LVCMOS33 } [get_ports {seg[5]}]
-#set_property -dict { PACKAGE_PIN U7   IOSTANDARD LVCMOS33 } [get_ports {seg[6]}]
+set_property -dict { PACKAGE_PIN W7   IOSTANDARD LVCMOS33 } [get_ports {seg[0]}]
+set_property -dict { PACKAGE_PIN W6   IOSTANDARD LVCMOS33 } [get_ports {seg[1]}]
+set_property -dict { PACKAGE_PIN U8   IOSTANDARD LVCMOS33 } [get_ports {seg[2]}]
+set_property -dict { PACKAGE_PIN V8   IOSTANDARD LVCMOS33 } [get_ports {seg[3]}]
+set_property -dict { PACKAGE_PIN U5   IOSTANDARD LVCMOS33 } [get_ports {seg[4]}]
+set_property -dict { PACKAGE_PIN V5   IOSTANDARD LVCMOS33 } [get_ports {seg[5]}]
+set_property -dict { PACKAGE_PIN U7   IOSTANDARD LVCMOS33 } [get_ports {seg[6]}]
 
 #set_property -dict { PACKAGE_PIN V7   IOSTANDARD LVCMOS33 } [get_ports dp]
 
-#set_property -dict { PACKAGE_PIN U2   IOSTANDARD LVCMOS33 } [get_ports {an[0]}]
-#set_property -dict { PACKAGE_PIN U4   IOSTANDARD LVCMOS33 } [get_ports {an[1]}]
-#set_property -dict { PACKAGE_PIN V4   IOSTANDARD LVCMOS33 } [get_ports {an[2]}]
-#set_property -dict { PACKAGE_PIN W4   IOSTANDARD LVCMOS33 } [get_ports {an[3]}]
+set_property -dict { PACKAGE_PIN U2   IOSTANDARD LVCMOS33 } [get_ports {an[0]}]
+set_property -dict { PACKAGE_PIN U4   IOSTANDARD LVCMOS33 } [get_ports {an[1]}]
+set_property -dict { PACKAGE_PIN V4   IOSTANDARD LVCMOS33 } [get_ports {an[2]}]
+set_property -dict { PACKAGE_PIN W4   IOSTANDARD LVCMOS33 } [get_ports {an[3]}]
 
 
 ##Buttons
 set_property -dict { PACKAGE_PIN U18   IOSTANDARD LVCMOS33 } [get_ports btnC]
-#set_property -dict { PACKAGE_PIN T18   IOSTANDARD LVCMOS33 } [get_ports btnU]
-#set_property -dict { PACKAGE_PIN W19   IOSTANDARD LVCMOS33 } [get_ports btnL]
-#set_property -dict { PACKAGE_PIN T17   IOSTANDARD LVCMOS33 } [get_ports btnR]
-#set_property -dict { PACKAGE_PIN U17   IOSTANDARD LVCMOS33 } [get_ports btnD]
+set_property -dict { PACKAGE_PIN T18   IOSTANDARD LVCMOS33 } [get_ports btnU]
+set_property -dict { PACKAGE_PIN W19   IOSTANDARD LVCMOS33 } [get_ports btnL]
+set_property -dict { PACKAGE_PIN T17   IOSTANDARD LVCMOS33 } [get_ports btnR]
+set_property -dict { PACKAGE_PIN U17   IOSTANDARD LVCMOS33 } [get_ports btnD]
 
 
 ##Pmod Header JA
--- src/top_module.v
+++ src/top_module.v
@@ -2,17 +2,122 @@
 
 // top_module module
 module top_module (
+    input btnU,
+    input btnD, 
+    input btnL, 
+    input btnR,
+    
     input clk,
     input sw0,
     input sw1,
-    output [7:0] Jx
+    output [7:0] Jx,
+    output reg [0:3] an = 4'b1111,
+    output reg [0:6] seg = 7'b1111111
 );
 
-bubble_sort bubble_sort_inst (
-    .clk(clk),
-    .sw0(sw0),
-    .sw1(sw1),
-    .Jx(Jx)
-);
+    // 7 seg display 
+    reg [16:0] seven_seg_counter = 0;
+    reg [1:0] anode_index = 0;
+    reg [3:0] sorting_algorithm = 0; // 0001 = bubble; 0010 = selection; 0100 = insertion; 1000 = qucick
+    
+    always @ (posedge clk) begin 
+        sorting_algorithm = btnU ? 4'b0001 : 
+                            btnD ? 4'b0010 :
+                            btnR ? 4'b0100 : 
+                            btnL ? 4'b1000 : sorting_algorithm;
+        seven_seg_counter <= seven_seg_counter + 1;
+        if (seven_seg_counter == 100_000) begin 
+            seven_seg_counter <= 0;
+            anode_index <= anode_index + 1;
+        end
+        if (sorting_algorithm == 4'b0001) begin // buuble sorting
+            case(anode_index) 
+                2'b00: begin 
+                    an = 4'b0111;
+                    seg = 7'b1110001;
+                end
+                2'b01: begin 
+                    an = 4'b1011;
+                    seg = 7'b1100000;
+                end
+                2'b10: begin 
+                    an = 4'b1101;
+                    seg = 7'b1000001;
+                end
+                2'b11: begin 
+                    an = 4'b1110;
+                    seg = 7'b1100000;
+                end
+            endcase
+        end
+        else if (sorting_algorithm == 4'b0010) begin //selection sorting
+            case (anode_index) 
+                2'b00: begin
+                    an = 4'b0111;
+                    seg = 7'b0110001;
+                end
+                2'b01: begin
+                    an = 4'b1011;
+                    seg = 7'b1110001;
+                end
+                2'b10: begin
+                    an = 4'b1101;
+                    seg = 7'b0110000;
+                end
+                2'b11: begin
+                    an = 4'b1110;
+                    seg = 7'b0100100;
+                end
+            endcase
+        end
+        else if (sorting_algorithm == 4'b0100) begin //insertion sorting
+            case (anode_index) 
+                2'b00: begin
+                    an = 4'b0111;
+                    seg = 7'b1111010;
+                end
+                2'b01: begin
+                    an = 4'b1011;
+                    seg = 7'b0100100;
+                end
+                2'b10: begin
+                    an = 4'b1101;
+                    seg = 7'b1101010;
+                end
+                2'b11: begin
+                    an = 4'b1110;
+                    seg = 7'b1001111;
+                end
+            endcase
+        end
+        else if (sorting_algorithm == 4'b1000) begin //quick sorting (?)
+            case (anode_index) 
+                2'b00: begin
+                    an = 4'b0111;
+                    seg = 7'b0110001;
+                end
+                2'b01: begin
+                    an = 4'b1011;
+                    seg = 7'b1001111;
+                end
+                2'b10: begin
+                    an = 4'b1101;
+                    seg = 7'b1000001;
+                end
+                2'b11: begin
+                    an = 4'b1110;
+                    seg = 4'b0001100;
+                end
+            endcase
+        end
+    end
+    
+
+    bubble_sort bubble_sort_inst (
+        .clk(clk),
+        .sw0(sw0),
+        .sw1(sw1),
+        .Jx(Jx)
+    );
 
 endmodule
